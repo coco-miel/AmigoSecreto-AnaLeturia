910c9ae8ef6c1c5fddc149764d404b0699aeb34a
Ana Ovejero Leturia <analeturia11@gmail.com>
2025-03-04 10:07:18 -0300

    Creo funcion sortearAmigo

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function agregarAmigo() {
     } else {
         alert("Por favor, inserte un nombre.");
     }
-}
+};
 
 function actualizarListaAmigos() {
     // Obtener el elemento ul
@@ -28,5 +28,25 @@ function actualizarListaAmigos() {
         const li = document.createElement("li");
         li.textContent = amigos[i];
         listaAmigos.appendChild(li);
+    }
+};
+
+function sortearAmigo() {
+    // Si hay al menos dos amigos, sortear uno
+    if (amigos.length >= 2) {
+        // Obtener un número aleatorio entre 0 y la cantidad de amigos
+        const indice = Math.floor(Math.random() * amigos.length);
+        const amigoSorteado = amigos[indice];
+        // Obtener el elemento ul
+        const resultado = document.getElementById("resultado");
+        // Limpiar la lista actual
+        resultado.innerHTML = "";
+        // Crear un nuevo elemento en la lista
+        const li = document.createElement("li");
+        li.textContent = amigoSorteado;
+        // Agregar el elemento a la lista
+        resultado.appendChild(li);
+    } else {
+        alert("No hay suficientes amigos para sortear.");
     }
 }
